44df579d2f356f80b795356fd5c1ec2f6c751a51
Thom Foxx <foxxthom@gmail.com>
2020-08-26 22:31:07 -0500

    EOS: Working on Rotation and Placement Issues

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -17,8 +17,11 @@ public class LevelManager : MonoBehaviour
     private Transform _joinedExit;
     private int _roomCount = 0;
     private Queue<GameObject> _rooms = new Queue<GameObject>();
-    [SerializeField]
+    [SerializeField][Range (0,100)]
     private int _exitChance = 50;
+
+    [SerializeField]//debugging tool
+    private int _roomLimit;
 
     #region Spin Radius Variables
     private float SpinRadius;
@@ -31,19 +34,23 @@ public class LevelManager : MonoBehaviour
         //Initial Room Spawn
         _currentRoom =Instantiate(_starterRooms[Random.Range(0, _starterRooms.Length - 1)], this.transform);
         _currentRoom.transform.parent = null;
+        _currentRoom.name = _roomCount.ToString();
+        _roomCount++;
         _rooms.Enqueue(_currentRoom);
         StartCoroutine(ExitSetup());
     }
 
     private IEnumerator ExitSetup()
     {
-        while (_currentRoom != null)
+        while (_currentRoom != null && _roomCount < _roomLimit)
         {
             _currentRoomScript = _currentRoom.GetComponent<LevelRoom>();
 
             foreach (Transform exit in _currentRoomScript.Exits)
             {
-                if (Random.Range(0, 100) >= _exitChance)
+                if (!exit.gameObject.activeInHierarchy)
+                    continue;
+                else if (Random.Range(0, 100) > _exitChance)
                     continue;
                 else
                 {
@@ -51,7 +58,7 @@ public class LevelManager : MonoBehaviour
                     NewRoom(exit);
                 }
             }
-            _rooms.Dequeue();
+            _rooms?.Dequeue();
             while (_rooms.Count > 0)
             {
                 if (_rooms.Peek() != null)
@@ -65,36 +72,69 @@ public class LevelManager : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
         yield return null;
+        Debug.Log("Generation Complete");
     }
 
     private void NewRoom(Transform exit)
     {
-        #region SpawnRandomRoom
+        int spawncheck = 0;
+
+
+#region SpawnRandomRoom
+Spawn:
+        spawncheck++;
+        int exitcheck = 0;
         _spawnRoomPrefab = _roomPrefabs[Random.Range(0, _roomPrefabs.Length - 1)];
-        _testingRoom = Instantiate(_spawnRoomPrefab, _currentRoom.transform.position, Quaternion.identity);
+        _testingRoom = Instantiate(_spawnRoomPrefab, _currentRoom.transform);
+        _testingRoom.name = _roomCount.ToString();
         _testingRoomScript = _testingRoom.GetComponent<LevelRoom>();
         #endregion
 
+        #region RandomExit
+        PickExit:
+        exitcheck++;
         _joinedExit = _testingRoomScript.Exits[Random.Range(0, _testingRoomScript.Exits.Length - 1)];
         _joinedExit.gameObject.SetActive(false);
-
+        #endregion
 
         #region MoveRoom To Join Exits
         RoomRotation(exit, _joinedExit);
-        float tempRot = (exit.rotation.eulerAngles.y + 180) - (_joinedExit.rotation.eulerAngles.y);
-        _testingRoom.transform.rotation = Quaternion.Euler(0, tempRot, 0);
-        #endregion
+        //float tempRot = (exit.rotation.eulerAngles.y + 180) + _testingRoom.transform.rotation.eulerAngles.y;
+        //_testingRoom.transform.rotation = Quaternion.Euler(0, tempRot, 0);
+#endregion
 
-        _rooms.Enqueue(_testingRoom);
+        if (!ColliderCheck(_testingRoom) && exitcheck < 10)
+        {
+            goto PickExit;
+        }
+        else if (!ColliderCheck(_testingRoom) && exitcheck > 10)
+        {
+            if (spawncheck < 10)
+                goto Spawn;
+            else
+            {
+                Destroy(_testingRoom.gameObject);
+                exit.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            _rooms.Enqueue(_testingRoom);
+            _testingRoom.transform.parent = null;
+            _testingRoom.transform.name = exit.name + " to " + _joinedExit.name;
+            _roomCount++;
+        }
     }
 
-    private bool ColliderCheck()
-    {        
-        Collide = _currentRoom.GetComponent<BoxCollider>();
+    private bool ColliderCheck(GameObject RoomToCheck)
+    {      
+        //returns TRUE if free of Collisions
+        Collide = RoomToCheck.GetComponent<BoxCollider>();
         Collide.enabled = true;
+        OtherColliders = default;
         GetSpinRadius();
 
-        OtherColliders = Physics.OverlapSphere(_currentRoom.transform.position, SpinRadius*2);
+        OtherColliders = Physics.OverlapSphere(RoomToCheck.transform.position, SpinRadius*2);
 
         foreach (Collider Other in OtherColliders)
         {
@@ -102,8 +142,11 @@ public class LevelManager : MonoBehaviour
                 continue;
             else if (Other == Collide)
                 continue;
+            else if (Other.transform == Collide.transform.parent)
+                continue;
             else if (Other.bounds.Intersects(Collide.bounds))
             {
+                Debug.Log("Collison Detected. " + Other.name);
                 return false;
             }
             else
@@ -149,107 +192,130 @@ public class LevelManager : MonoBehaviour
 
         float TempX = Offset.x;
         float TempZ = Offset.z;
-       
+        int TempRot = 0;
+
         if (Mathf.Round(Exit.rotation.eulerAngles.y) == 0)
         {
-            switch (Mathf.Round(JoinedExit.localRotation.eulerAngles.y))
+            switch (Mathf.Round(JoinedExit.rotation.eulerAngles.y))
             {
                 case 0:
                     TempVector.x = TempVector.x + TempX;
                     TempVector.z = TempVector.z + TempZ;
+                    TempRot = 180;
                     break;
                 case 90:
                     TempVector.x = TempVector.x - TempZ;
                     TempVector.z = TempVector.z + TempX;
+                    TempRot = 90;
                     break;
                 case 180:
                     TempVector.x = TempVector.x - TempX;
                     TempVector.z = TempVector.z - TempZ;
+                    TempRot = 0;
                     break;
                 case 270:
                     TempVector.x = TempVector.x + TempZ;
                     TempVector.z = TempVector.z - TempX;
+                    TempRot = -90;
                     break;
                 default:
-                    Debug.Log("Something is going wrong");
+                    Debug.Log("Something went Wrong. Rot0");
                     break;
             }
         }
-        if (Mathf.Round(Exit.rotation.eulerAngles.y) == 90)
+        else if (Mathf.Round(Exit.rotation.eulerAngles.y) == 90)
         {
-            switch (Mathf.Round(JoinedExit.localRotation.eulerAngles.y))
+            switch (Mathf.Round(JoinedExit.rotation.eulerAngles.y))
             {
                 case 0:
                     TempVector.x = TempVector.x + TempZ;
                     TempVector.z = TempVector.z - TempX;
+                    TempRot = -90;
                     break;
                 case 90:
                     TempVector.x = TempVector.x + TempX;
                     TempVector.z = TempVector.z + TempZ;
+                    TempRot = 180;
                     break;
                 case 180:
                     TempVector.x = TempVector.x - TempZ;
                     TempVector.z = TempVector.z - TempX;
+                    TempRot = 90;
                     break;
                 case 270:
                     TempVector.x = TempVector.x - TempX;
                     TempVector.z = TempVector.z - TempZ;
+                    TempRot = 0;
                     break;
                 default:
+                    Debug.Log("Something went Wrong. Rot90");
                     break;
             }
         }
-        if (Mathf.Round(Exit.rotation.eulerAngles.y) == 180 || Mathf.Round(Exit.rotation.eulerAngles.y) == -180)
+        else if (Mathf.Round(Exit.rotation.eulerAngles.y) == 180)
         {
-            switch (Mathf.Round(JoinedExit.localRotation.eulerAngles.y))
+            switch (Mathf.Round(JoinedExit.rotation.eulerAngles.y))
             {
                 case 0:
                     TempVector.x = TempVector.x - TempX;
                     TempVector.z = TempVector.z - TempZ;
+                    TempRot = 0;
                     break;
                 case 90:
                     TempVector.x = TempVector.x - TempZ;
                     TempVector.z = TempVector.z - TempX;
+                    TempRot = 270;
                     break;
                 case 180:
                     TempVector.x = TempVector.x + TempX;
                     TempVector.z = TempVector.z + TempZ;
+                    TempRot = 180;
                     break;
                 case 270:
                     TempVector.x = TempVector.x + TempZ;
                     TempVector.z = TempVector.z + TempX;
+                    TempRot = 90;
                     break;
                 default:
+                    Debug.Log("Something went Wrong. Rot180");
                     break;
             }
         }
-        if (Mathf.Round(Exit.rotation.eulerAngles.y) == 270 || Mathf.Round(Exit.rotation.eulerAngles.y) == -90)
+        else if (Mathf.Round(Exit.rotation.eulerAngles.y) == 270 || Mathf.Round(Exit.rotation.eulerAngles.y) == -90)
         {
-            switch (Mathf.Round(JoinedExit.localRotation.eulerAngles.y))
+            switch (Mathf.Round(JoinedExit.rotation.eulerAngles.y))
             {
                 case 0:
                     TempVector.x = TempVector.x - TempZ;
                     TempVector.z = TempVector.z + TempX;
+                    TempRot = 180;
                     break;
                 case 90:
                     TempVector.x = TempVector.x - TempX;
                     TempVector.z = TempVector.z - TempZ;
+                    TempRot = 0;
                     break;
                 case 180:
                     TempVector.x = TempVector.x + TempZ;
                     TempVector.z = TempVector.z - TempX;
+                    TempRot = -90;
                     break;
                 case 270:
                     TempVector.x = TempVector.x - TempX;
                     TempVector.z = TempVector.z + TempZ;
+                    TempRot = 90;
                     break;
                 default:
+                    Debug.Log("Something went Wrong. Rot270");
                     break;
             }
         }
-        Debug.Log(Mathf.Round(Exit.rotation.eulerAngles.y));
-        Debug.Log(TempVector);
-        _testingRoom.transform.position = TempVector; ///New Room Position
-    }
+        else
+        {
+            Debug.Log("Problem with Rotation " + Mathf.Round(Exit.rotation.eulerAngles.y) + " " + Mathf.Round(JoinedExit.rotation.eulerAngles.y));
+        }
 
+        _testingRoom.transform.position = TempVector; ///New Room Position
+        _testingRoom.transform.rotation = Quaternion.Euler(0, TempRot, 0);
+    }
 }
